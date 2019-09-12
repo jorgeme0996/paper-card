398324c183ab1cc1cb30037f18cfea21864f8f0f
Jorge Martinez <36254865+jorgeme0996@users.noreply.github.com>
2019-09-12 13:10:32 -0500

    Initial commit

---
+++ paper-card.js
@@ -0,0 +1,68 @@
+import { html, LitElement, css } from 'lit-element';
+
+import '@polymer/paper-button'
+
+class PaperCard extends LitElement {
+    static get properties() {
+        return {
+            img: {type: String},
+            buttons: {type: Array},
+            description: {type: String},
+            title: {type: String},
+        };
+    }
+
+  static get styles() {
+    return css`
+    :host {
+      display: block;
+    }
+    div.card {
+        width: 300px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        text-align: center;
+        border-radius: 10px;
+    }
+    div.header {
+        color: white;
+        padding: 10px;
+        font-size: 40px;
+    }
+    img{
+        height: 100%;
+        width: 100%;
+    }
+    div.container {
+        padding: 10px;
+    }
+    `
+  }
+
+    updated(changedProps) {
+        if (changedProps.has('buttons')) {
+
+        
+        }
+    }
+
+    render() {
+        return html`
+            <div class="card">
+                <div class="header">
+                    <img src="${this.img}">
+                </div>
+                <div class="container">
+                    <h2>${this.title}</h2>
+                    <p>${this.description}</p>
+                    ${this.buttons ? html`
+                        ${this.buttons.map(button=>html`
+                            <paper-button>${button.text}</paper-button>
+                        `)}
+                    `: ''}
+                </div>
+            </div>
+        `;
+    }
+}
+
+window.customElements.define("paper-card", PaperCard);
